25b25b86f07560904ccb07a2e9f5d98bac3d4423
justinsuter-qiKFlV <suter765@gmail.com>
2022-02-24 13:13:38 -0800

    finished questions

---
+++ assets/js/questions.js
@@ -0,0 +1,113 @@
+// list of all questions, choices, and answers, 
+var questions = [
+    {
+      title: "When a user views a page that utilizes Javascript, what is actually executing the script?",
+      choices: [ 
+          "Web browser server", 
+          "The Executioner", 
+          "The User's machine running the web browser",
+          "All of the above"
+        ],
+      answer: "The User's machine running the web browser"
+    },
+
+    {
+      title: "What are variables used for in Javascript?",
+      choices: [
+        "Storing numbers, dates, or other values",
+        "Varying randomly",
+        "Causing high-school algebra flashbacks",
+        "None of the above"
+      ],
+      answer: "Storing numbers, dates, or other values"
+    },
+
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: [
+          "strings", 
+          "booleans", 
+          "alerts", 
+          "numbers"
+        ],
+      answer: "alerts"
+    },
+
+    {
+      title: "Which of the following are capabilities of functions in JavaScript?",
+      choices: [
+        "Accept parameters and Return a value",
+        "Return a value",
+        "Accept parameters",
+        "None of the above"
+      ],
+      answer: "Accept parameters"
+    },
+
+    {
+      title:
+        "Which one of these is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Load time errors",
+        "Run time errors",
+        "Animation time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
+    },
+
+    {
+      title: "Which is an extension to HTML that can enclose Javascript statements?",
+      choices: [
+        "<title>",
+        "<script>",
+        "<body>",
+        "<head>"
+      ],
+      answer: "<script>"
+    },
+
+    {
+      title: "What's the proper syntax for referring to an external script called 'abc.js'?",
+      choices: [
+          "<script href='abc.js'>", 
+          "<script src='abc.js'>", 
+          "<script name='abc.js'>", 
+          "<script refer='abc.js'>"
+        ],
+      answer: "<script src='abc.js'>"
+    },
+
+    {
+      title: "Javascript entities start with _____ and end with ____",
+      choices: [
+          "Semicolon / colon",
+          "Semicolon / ampersand",
+          "Ampersand / colon",
+          "Ampersand / semicolon"
+      ],
+      answer: "Ampersand / semicolon"
+    },
+
+    {
+      title: "Which statement best describes Javascript",
+      choices: [
+          "a low-level programming language", 
+          "an object-oriented scripting language", 
+          "a compiled scripting language", 
+          "an scripting language precompiled in the browser"
+        ],
+      answer: "an object-oriented scripting language"
+    },
+
+    {
+      title: "You can nest functions in Javascript.",
+      choices: [
+          "True", 
+          "False"
+        ],
+      answer: "True"
+    },
+    
+  ];
+  
